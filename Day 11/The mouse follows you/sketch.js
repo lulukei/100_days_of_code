fb521cbaf5e8e3c94221e6c177c3aa481b4926e5
lulukei <mathilde.dongala@gmail.com>
2017-12-18 19:20:05 +0100

    Drawing with the mouse

---
+++ Day 11/The mouse follows you/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(800, 600);
+  noStroke();
+  fill(160, 158, 242, 80);
+}
+
+function draw() {
+  background(242, 201, 158);
+  ellipse(mouseX, mouseY, 20, 20);
+}
---
+++ Day 11/Track the mouse/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(800, 600);
+  noStroke();
+  background(242, 201, 158);
+  fill(160, 158, 242, 80);
+}
+
+function draw() {
+  ellipse(mouseX, mouseY, 20, 20);
+}
