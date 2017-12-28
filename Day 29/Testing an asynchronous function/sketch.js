43f1fc0adeba2bdf56252897e327e0ed15e1f46d
lulukei <mathilde.dongala@gmail.com>
2017-12-28 23:43:41 +0100

    Testing an asynchronous function

--- Day 29/Testing an asynchronous function/sketch.js
+++ Day 29/Testing an asynchronous function/sketch.js
@@ -1,12 +1,8 @@
-var img;
-
 function setup() {
-  createCanvas(480, 120);
-  img = loadImage("clouds.png");
+  createCanvas(1300, 800);
 }
 
-function draw() {
-    noLoop();
-    background(129, 222, 102);
-    image(img, 0, 0);
+function drawImage() {
+  background(44, 462, 11);
+  text("Vais-je functionner?", 400, 400);
 }
