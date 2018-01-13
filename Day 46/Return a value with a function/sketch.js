1abb49895e8ac7362ee6fff873dd5aa216972d80
lulukei <mathilde.dongala@gmail.com>
2018-01-13 19:34:07 +0100

    Return a value with a function

--- Day 46/Return a value with a function/sketch.js
+++ Day 46/Return a value with a function/sketch.js
@@ -1,13 +1,13 @@
 function setup(){
   var height_in_cms = 174;
-  var height_in_feet = calculateFeet(height_in_cms);
-  print(height_in_feet);
-  var height_in_feet = 5.2;
-  var height_in_cms = calculateCms(height_in_feet);
+  var height_in_foot = calculatefoot(height_in_cms);
+  print(height_in_foot);
+  var height_in_foot = 5.2;
+  var height_in_cms = calculateCms(height_in_foot);
   print(height_in_cms);
 }
 
-function calculateFeet(h) {
+function calculatefoot(h) {
   var newHeight = h * 0.032808;
   return newHeight;
 }
