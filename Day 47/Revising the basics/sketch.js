7e87b8ddb4a297da6abd9b7483c82950cf3cb23c
lulukei <mathilde.dongala@gmail.com>
2018-01-14 18:17:25 +0100

    Revising the basics

---
+++ Day 47/Revising the basics/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(1300, 800);
+}
+
+function draw() {
+  strokeWeight(20);
+  line(650, 400, 575, 500);
+  line(650, 400, 650, 550);
+  line(650, 400, 725, 500);
+  strokeWeight(6);
+  fill(255);
+  ellipse(650, 400, 180, 180);
+  fill(93, 155, 182);
+  ellipse(650, 400, 100, 100);
+  fill(0);
+  ellipse(650, 400, 25, 25);
+}
