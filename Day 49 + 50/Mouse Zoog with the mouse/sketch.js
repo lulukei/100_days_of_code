422f6d489498b5f8817563f2b0a6262866091feb
lulukei <mathilde.dongala@gmail.com>
2018-01-18 23:53:56 +0100

    Adding images on website (HTML) and moving a shape with the mouse (p5)

---
+++ Day 49 + 50/Mouse Zoog with the mouse/sketch.js
@@ -0,0 +1,23 @@
+x = 650;
+y = 400;
+
+function setup() {
+  createCanvas(1300, 800);
+}
+
+function draw() {
+  background(255);
+  x = mouseX;
+  y = mouseY;
+  strokeWeight(20);
+  line(x, y, x-75, y +100);
+  line(x, y, x, y + 150);
+  line(x, y, x + 75, y + 100);
+  strokeWeight(6);
+  fill(255);
+  ellipse(x, y, 180, 180);
+  fill(93, 155, 182);
+  ellipse(x, y, 100, 100);
+  fill(0);
+  ellipse(x, y, 25, 25);
+}
