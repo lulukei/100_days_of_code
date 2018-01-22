8ce08489c2409e7fc65dd11cc6cda4a2cc75460b
lulukei <mathilde.dongala@gmail.com>
2018-01-22 21:43:03 +0100

    exercise with conditionals and logical operators

---
+++ Day 55/Conditional and logical operators/sketch.js
@@ -0,0 +1,7 @@
+// // //Exercise
+// // Are the following expressions true or false? Assume variable x  = 5 and int y = 6
+// //
+// // !(x>6) is true
+// (x == 6 && x = 5) is false
+// (x == 6 || x ==5) is true
+// (x > -1 77 y < 10) is true 
