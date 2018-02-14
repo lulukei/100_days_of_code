7b32fcd5bc0054aa37d0e97aa3d3374355965a77
lulukei <mathilde.dongala@gmail.com>
2018-02-14 14:26:56 +0100

    First function exercise

--- Day 69/First function exercises/sketch.js
+++ Day 69/First function exercises/sketch.js
@@ -1,26 +1,12 @@
-var y = 100;
+// What functions might you write for your Lesson Two Project?
+drawZoog();
+animateZoog();
+makeZoogTalk();
 
-function setup() {
-  createCanvas(1300, 800);
-}
-
-function draw() {
-
-  for (x = width/15; x < width - 100; x += 200) {
-
-  // Legs
-  strokeWeight(20);
-  line(x, y, x*0.9, y*1.25);
-  line(x, y, x, y*1.25);
-  line(x, y, x*1.09, y*1.25);
-
-  //Body
-  strokeWeight(6);
-  fill(255);
-  ellipse(x, y, 180, 180);
-  fill(93, 155, 182);
-  ellipse(x, y, 100, 100);
-  fill(0);
-  ellipse(x, y, 25, 25);
-  }
-}
+//What functions might you write in order to program the game Pong?
+background();
+drawMiddleLine();
+drawRackets();
+drawBalls();
+moveRackets();
+moveBall();
