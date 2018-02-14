6b19d625e799046800131a656fd9d83f110bae2b
lulukei <mathilde.dongala@gmail.com>
2018-02-14 14:27:14 +0100

    Second function exercise

--- Day 70/ Second function exercise/sketch.js
+++ Day 70/ Second function exercise/sketch.js
@@ -1,26 +1,8 @@
-var y = 100;
+// Take any Processing program you have written and modularize it using functions, as above.
 
-function setup() {
-  createCanvas(1300, 800);
-}
-
-function draw() {
-
-  for (x = width/15; x < width - 100; x += 200) {
-
-  // Legs
-  strokeWeight(20);
-  line(x, y, x*0.9, y*1.25);
-  line(x, y, x, y*1.25);
-  line(x, y, x*1.09, y*1.25);
-
-  //Body
-  strokeWeight(6);
-  fill(255);
-  ellipse(x, y, 180, 180);
-  fill(93, 155, 182);
-  ellipse(x, y, 100, 100);
-  fill(0);
-  ellipse(x, y, 25, 25);
-  }
-}
+displayRect1();
+displayRect2();
+changeColourRect1();
+ChangeColourRect2();
+bounceDirectionC1();
+bounceDirectionC2();
