14aa633d6edebea3e3136138cab08905347b57f3
lulukei <mathilde.dongala@gmail.com>
2018-02-14 14:27:36 +0100

    A boring function exercise

--- Day 73/A boring function exercise/sketch.js
+++ Day 73/A boring function exercise/sketch.js
@@ -1,26 +1,9 @@
-function setup(){
-  createCanvas(1300, 800);
-  ellipseMode(CENTER);
-}
+//Write a function that takes one argument = F for Fahrenhet - and
+//and computes the result of the following equation
 
-function draw(){
-  background(255, 78, 125);
-  drawFlower(1000, 600, 100, 100, 80, 155);
-  drawFlower(800, 400, 60, 60, 32, 87);
-  drawFlower(500, 500, 140, 140, 250, 56);
-  drawFlower(200, 300, 100, 100, 100, 20);
-  drawFlower(1200, 200, 20, 20, 38, 200);
-  drawFlower(650, 200, 120, 120, 38, 28);
+//Formula : C = (F - 32) * (5/9)
 
-}
-
-function drawFlower(x, y, w, h, b, g) {
-  var offset = h/2;
-  fill(0, 0, b);
-  ellipse(x - offset, y - offset, w, h);
-  ellipse(x - offset, y + offset, w, h);
-  ellipse(x + offset, y + offset, w, h);
-  ellipse(x + offset, y - offset, w, h);
-  fill(0, g, 0);
-  ellipse(x, y, w, h);
+function convertToCelsius(float F) {
+  float C = (F - 32) * (5/9);
+  return C;
 }
