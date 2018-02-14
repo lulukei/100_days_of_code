3f5faef94e7ae66f541e8fef01bcce529d91f663
lulukei <mathilde.dongala@gmail.com>
2018-02-14 14:26:01 +0100

    Move the position of the lines randomly

--- Day 38/Move the position of the lines randomly/sketch.js
+++ Day 38/Move the position of the lines randomly/sketch.js
@@ -1,15 +1,3 @@
-// function setup() {
-//   createCanvas(480, 120);
-//   strokeWeight(2);
-// }
-//
-// function draw() {
-//   background(204);
-//   for (var i = 20; i < 400; i += 20) {
-//     line(i, 0, i + i/2, 80);
-//   }
-// }
-
 function setup(){
   createCanvas(1300, 800);
   strokeWeight(20);
