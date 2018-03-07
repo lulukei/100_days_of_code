159475165b4f8f4f8165bad25d3ae378e7f0e0ca
lulukei <mathilde.dongala@gmail.com>
2018-03-07 14:33:33 +0100

    twitter app set up

---
+++ Day 99/node2/bot.js
@@ -0,0 +1,4 @@
+console.log('the bot is starting!');
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
---
+++ Day 99/node2/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key: 'yjm5PnbWWJVpAWtTOeu7W1HDI',
+  consumer_secret: '9wKC0CkE5EQ14ZpneE4bamtYxQlGnIH4tjMSz5zNieHtLreg7l',
+  access_token: '971127506215620608-G2GXFa7X4Ccekr7vNw5dn0uCOZQxl0d',
+  access_token_secret: 'cwBPva1nG27gMGpLmOnHEm2BdNytpTXsFSeXWV94ltftR',
+  timeout_ms: 60*1000, // optional HTTP request timeout to apply to all requests.
+}
